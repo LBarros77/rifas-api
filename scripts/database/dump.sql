423134186af1985412a62c21647ad500b0891ecd
Leonardo Barros <leonprogramer@gmail.com>
2024-03-04 16:10:28 -0300

    Fict: Improvement in dump code

--- scripts/database/dump.sql
+++ scripts/database/dump.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS "Users" (
   id VARCHAR PRIMARY KEY,
   name VARCHAR NOT NULL,
   phone_number VARCHAR(11) NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS Users (
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Products (
+CREATE TABLE IF NOT EXISTS "Products" (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   price DECIMAL,
   status VARCHAR,
   quantity INTEGER NOT NULL,
-  processed INTEGER NOT NULL,
+  processed INTEGER DEFAULT 0 NOT NULL,
   type_raffles VARCHAR,
   favorite BOOLEAN DEFAULT FALSE,
   game_mode VARCHAR,
@@ -28,47 +28,50 @@ CREATE TABLE IF NOT EXISTS Products (
   user_id VARCHAR,
   draw_date TIMESTAMPTZ,
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY(user_id) REFERENCES Users(id)
+  FOREIGN KEY(user_id) REFERENCES "Users"(id)
 );
 
-CREATE TABLE IF NOT EXISTS PaymentPix (
-  key_pix VARCHAR NOT NULL,
-  participant_id iNTEGER NOT NULL UNIQUE,
-  FOREIGN KEY(participant_id) REFERENCES Participate(id)
-);
-
-CREATE TABLE IF NOT EXISTS Order (
-  participant_id INTEGER PRIMARY KEY,
-  key_pix VARCHAR,
-  dates VARCHAR,
-  price DECIMAL DEFAULT 0.0,
-  FOREIGN KEY(participant_id) REFERENCES Participate(id)
-);
-
-CREATE TABLE IF NOT EXISTS Participate (
+CREATE TABLE IF NOT EXISTS "Participate" (
   id SERIAL PRIMARY KEY,
-  number_list VARCHAR NOT NULL,
+  numbers VARCHAR NOT NULL,
   reserveds INTEGER NOT NULL,
   payeds INTEGER NOT NULL,
   conferred BOOLEAN DEFAULT TRUE,
-  FOREIGN KEY(number_list) REFERENCES Products(participate),
-  FOREIGN KEY(reserveds) REFERENCES Order(participant),
-  FOREIGN KEY(payeds) REFERENCES Raffles(participant),
-  FOREIGN KEY(conferred) REFERENCES PaymentPix(participant),
-  UNIQUE(number_list, reserveds, conferred),
+  UNIQUE(numbers, reserveds, conferred),
   CHECK(conferred in (TRUE, FALSE))
 );
 
-CREATE TABLE IF NOT EXISTS Raffles (
-  product_id INTEGER,
-  participant_id INTEGER,
-  PRIMARY KEY(product_id, participant_id)
-  FOREIGN KEY(product_id) REFERENCES Products(id),
-  FOREIGN KEY(participant_id) REFERENCES Participate(id),
+CREATE TABLE IF NOT EXISTS "Order" (
+  participant_id INTEGER PRIMARY KEY,
+  key_pix VARCHAR,
+  datas VARCHAR,
+  price DECIMAL DEFAULT 0.0,
+  FOREIGN KEY(participant_id) REFERENCES "Participate"(id)
 );
 
+CREATE TABLE IF NOT EXISTS "PaymentPix" (
+  id SERIAL PRIMARY KEY,
+  key_pix VARCHAR NOT NULL,
+  participant_id iNTEGER NOT NULL,
+  FOREIGN KEY(participant_id) REFERENCES "Participate"(id)
+);
+
+CREATE TABLE IF NOT EXISTS "Raffles" (
+  product_id INTEGER,
+  participant_id INTEGER,
+  PRIMARY KEY(product_id, participant_id),
+  FOREIGN KEY(product_id) REFERENCES "Products"(id),
+  FOREIGN KEY(participant_id) REFERENCES "Participate"(id)
+);
+
+-- Alter table
+ALTER TABLE "Participate"
+  ADD CONSTRAINT fk_reserveds_order
+  FOREIGN KEY(reserveds)
+  REFERENCES "Order"(participant_id);
+
 -- Seed
-INSERT INTO users (
+INSERT INTO "Users" (
   id,
   name,
   phone_number,
@@ -126,4 +129,4 @@ INSERT INTO Products (
   '2024-3-11 08:43:21'
 );
 
-SELECT * FROM users;
+SELECT * FROM Users;
